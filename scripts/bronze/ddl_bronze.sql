33c1fe503bd1c6fb736d91e9813dd03359f7ac9f
ariel <repobyariel@gmail.com>
2025-06-14 12:03:27 +0700

    clean up the code

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,19 +8,22 @@ warning:
 
 if OBJECT_ID('bronze.crm_cust_info','U')is not null
 	drop table bronze.crm_cust_info;
+go
 
 create table bronze.crm_cust_info(
-cst_id	int,
-cst_key nvarchar(50),
-cst_firstname nvarchar(50),
-cst_lastname nvarchar(50),
-cst_marital_status nvarchar(50),
-cst_gndr nvarchar(50),
-cst_create_date date
+cst_id				int,
+cst_key				nvarchar(50),
+cst_firstname		nvarchar(50),
+cst_lastname		nvarchar(50),
+cst_marital_status	nvarchar(50),
+cst_gndr			nvarchar(50),
+cst_create_date		date
 );
+go
 
 if OBJECT_ID('bronze.crm_prd_info','U')is not null
 	drop table bronze.crm_prd_info;
+go
 
 create table bronze.crm_prd_info(
 prd_id			int,
@@ -32,47 +35,53 @@ prd_line		nvarchar(50),
 prd_start_dt	date,
 prd_end_dt		date
 );
+go
 
 if OBJECT_ID('bronze.crm_sales_details','U')is not null
 	drop table bronze.crm_sales_details;
+go
 
 create table bronze.crm_sales_details(
-sls_ord_num nvarchar(50),
-sls_prd_key nvarchar(50),
-sls_cust_id int,
-sls_order_dt date,
-sls_ship_dt date,
-sls_due_dt date,
-sls_sales int,
-sls_quantity int,
-sls_price int,
-dwh_create_date datetime default getdate()
+sls_ord_num		nvarchar(50),
+sls_prd_key		nvarchar(50),
+sls_cust_id		int,
+sls_order_dt	date,
+sls_ship_dt		date,
+sls_due_dt		date,
+sls_sales		int,
+sls_quantity	int,
+sls_price		int
 );
+go
 
 if OBJECT_ID('bronze.erp_cust_az12','U')is not null
 	drop table bronze.erp_cust_az12;
+go
 
 create table bronze.erp_cust_az12(
-cid nvarchar(50),
-bdate date,
-gen nvarchar(50),
-
+cid		nvarchar(50),
+bdate	date,
+gen		nvarchar(50)
 );
+go
 
 if OBJECT_ID('bronze.erp_loc_a101','U')is not null
 	drop table bronze.erp_loc_a101;
-
+go
 create table bronze.erp_loc_a101(
-cid nvarchar(50),
-cntry nvarchar(50) 
+cid		nvarchar(50),
+cntry	nvarchar(50) 
 );
+go
 
 if OBJECT_ID('bronze.erp_px_cat_g1v2','U')is not null
 	drop table bronze.erp_px_cat_g1v2;
+go
 
 create table bronze.erp_px_cat_g1v2(
-id nvarchar(50),
-cat nvarchar(50),
-subcat nvarchar(50),
-maintenance nvarchar(50)
+id			nvarchar(50),
+cat			nvarchar(50),
+subcat		nvarchar(50),
+maintenance	nvarchar(50)
 );
+go
